f402b1706ef307e053d9a0839d17cab2c5c2196a
zmant <zmant724@aliyun.com>
2019-11-16 00:22:44 +0800

    Get the url of song Successfully

--- src/app.rs
+++ src/app.rs
@@ -16,36 +16,27 @@ fn index_root() -> impl Responder {
     HttpResponse::Ok().body("Hello World!")
 }
 
-async fn search_song(value: &SongInfo) -> Result<String, reqwest::Error>  {
+fn search_song(value: &SongInfo) -> String {
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, "application/x-www-form-urlencoded".parse().unwrap());
     headers.insert(USER_AGENT, "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36".parse().unwrap());
     headers.insert(REFERER, "https://music.163.com".parse().unwrap());
 
-    let client = reqwest::ClientBuilder::new()
+    let client = reqwest::blocking::ClientBuilder::new()
         .default_headers(headers)
         .build()
         .unwrap();
 
-    let (params, encSecKey) = Crypto::weapi(&value.to_string());
-//    let body = format!("params={}encSecKey={}", params, encSecKey);
-
-    let body = "W%2BS8Ho5Tlwj1Kp3vkC6b%2F1aZTP2bDmVZo%2FNJdtVmrs0Pd6yMEKJ9vo4hCLSRoFKmlR1JDk5lXfHjEN0XChQr60CFzf9OEeK7S%2FBIOwbg9w%3D&encSecKey=a15a23acd13c3112b850d70c9d3d6dceb5bddde7e329cfc9443ff03dd761e151f088e8cbbdda199021110e9084ae784f4a79d0dae84acc5da500743c9abb8b8002b3d7ed7680079f3ff132aa1b0ebdf91e94e8b513fcdea7715b0ee87d31374b50d9ecf0d1c5581c09ebed7b7a3052fc925e661d3304e96d090fcab37b99c517";
-
-    println!("body={}", body);
-//    let res = "hello".to_string();
+    let body = Crypto::weapi(&value.to_string());
 
     let res = client.post("https://music.163.com/weapi/song/enhance/player/url")
-        .json(&serde_json::json!({
-            "params": params,
-            "encSecKey": encSecKey
-        }))
-        .send().await?
-        .text().await?;
+        .body(body)
+        .send().unwrap()
+        .text().unwrap();
 
-    println!("resssssssssss = {:?}", res);
+    println!("{:?}", res);
 
-    Ok(res)
+    res
 }
 
 #[derive(Deserialize)]
@@ -63,12 +54,9 @@ impl ToString for SongInfo {
 
 fn index_song(info: web::Query<SongInfo>) -> impl Responder {
 
-    println!("id={}, br={}", info.id, info.br);
-
     let value = info.into_inner();
 
-    let res = block_on(search_song(&value))
-        .unwrap_or("hello world!".to_string());
+    let res = search_song(&value);
 
     HttpResponse::Ok().body(res)
 }
@@ -82,7 +70,7 @@ pub fn start_server() {
                     .data(
                         web::Query::<SongInfo>::configure(|cfg| {
                             cfg.error_handler(|err, _req| {
-                                println!("Internal Error!!");
+                                println!("Internal Error!! errInfo={}", err);
                                 error::InternalError::from_response(
                                     err,
                                     HttpResponse::Conflict().finish(),
@@ -93,7 +81,6 @@ pub fn start_server() {
                     ).route(web::get().to(index_song))
             )
             .route("/", web::get().to(index_root))
-            //.route("/song/url", web::get().to(index_song))
     })
     .bind("localhost:8000").unwrap()
     .run().unwrap();
--- src/crypto.rs
+++ src/crypto.rs
@@ -4,6 +4,7 @@ use rand::rngs::OsRng;
 use openssl::rsa::{ Rsa, Padding, };
 use openssl::symm::{ encrypt, Cipher, };
 use rand::RngCore;
+use urlqstring::querystring;
 
 const iv: &str = "0102030405060708";
 const preset_key: &str = "0CoJUm6Qyw8W8jud";
@@ -13,7 +14,7 @@ const rsa_public_key: &str = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQ
 pub struct Crypto;
 
 impl Crypto {
-    pub fn weapi(text: &str) ->(String, String) {
+    pub fn weapi(text: &str) -> String {
         let mut secret_key = [0u8; 16];
         OsRng.fill_bytes(&mut secret_key);
         let base_62 = base62.to_string().into_bytes();
@@ -23,7 +24,7 @@ impl Crypto {
 
         let params = Crypto::aes_encrypt(
             &Crypto::aes_encrypt(text, preset_key),
-            std::str::from_utf8(&key).unwrap(),
+            std::str::from_utf8(&key).unwrap()
         );
 
         let enc_sec_key = Crypto::rsa_encrypt(
@@ -31,7 +32,10 @@ impl Crypto {
             rsa_public_key
         );
 
-        (params, enc_sec_key)
+        querystring::stringify(vec![
+            ("params", &params),
+            ("encSecKey", &enc_sec_key)
+        ])
     }
 
     pub fn aes_encrypt(data: &str, key: &str) -> String {
@@ -66,20 +70,27 @@ mod tests {
 
     use super::Crypto;
     use crate::crypto::{preset_key, rsa_public_key};
+    use base64::CharacterSet::Crypt;
 
     #[test]
     fn test_aes_encrypt() {
         let msg = r#"{"ids":"[347230]","br":999000}"#;
 
+        let key1 = "gLiwKFot44HYFRAy";
+
         let res = Crypto::aes_encrypt(msg,  preset_key);
 
         assert_eq!(res, "pgHP1O/hr+IboRMAq6HzpHjyYwNlv1x0G4BBjd1ohdM=");
+
+        let res2 = Crypto::aes_encrypt(&res, key1);
+
+        assert_eq!(res2, "3EC4ojigTl0OgjyYtcd+97P7YKarculWrOxSgNO5clkQftvO1jOvS8aAhK6diyOb");
     }
 
     #[test]
     fn test_rsa_encrypt() {
-        let msg = "hello world!";
-        let res = Crypto::rsa_encrypt(msg, rsa_public_key);
-        assert_eq!(res, "7979be697beebc8735f1693a4f76a4dd4f52bae1d5fb923e07dfa70565270528da94123b9a988fde402f66fc27e66c2deb456d9581e495f73e238f83f8f7bb7127bf9230def9b35385b43b2f08c32e00f4435af69c4d3a2b3d4e327343fcccd662abad1057a4a6262828863bb6bf14f6af8f972b22a3c58e47683541772a19ae");
+        let key2 = "yARFYH44toFKwiLg";
+        let res = Crypto::rsa_encrypt(key2, rsa_public_key);
+        assert_eq!(res, "5ff8bdb3ed3dd15a26e9025e9abcff0d7a3764dafbc70e33859a892584c681f1aab314b8ad1f3418650ff851bdb0685fc5136a88e059c592da104bbeaba666fbe89eb405c7b66eab4db8ee3ab13a3f98cb41b2ac9981ed4e441ed8e1870524d001ee6ebc1c09f7a945677e5b56a3e964a224c3ee75ac43fbf513f6a8bf7472ee");
     }
 }
